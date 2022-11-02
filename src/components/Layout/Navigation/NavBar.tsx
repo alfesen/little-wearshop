2761ab27d45d4d5324da62109ef1a9e0a2f3714e
alfesen <89510380+alfesen@users.noreply.github.com>
2022-11-02 17:25:42 +0100

    Add cart button to nav

--- src/components/Layout/Navigation/NavBar.tsx
+++ src/components/Layout/Navigation/NavBar.tsx
@@ -1,13 +1,14 @@
 import s from './NavBar.module.scss'
 import NavBarLinks from './NavBarLinks/NavBarLinks'
 import Logo from './Logo/Logo'
+import CartButton from './CartButton/CartButton'
 const NavBar = () => {
   return (
     <nav>
       <div className={`container ${s.nav}`}>
         <NavBarLinks />
-        <Logo className={s.logo}/>
-        <div>cartButton</div>
+        <Logo className={s.logo} />
+        <CartButton />
       </div>
     </nav>
   )
